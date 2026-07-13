7a5f09f177190a768ea859c994d4d95291181290
nati88kid <106956292+nati88kid@users.noreply.github.com>
2026-07-13 22:51:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-self.addEventListener('install', (e) => { 
-  console.log('App Installed'); 
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
 });
 
-// NEW: We added a "fetch" listener. 
-// This proves to the phone that we are a real, secure app!
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker: Activated');
+});
+
+// Android requires a fetch listener to trigger the "Real App" installation
 self.addEventListener('fetch', (e) => {
-  // We leave this blank for now. The phone just needs to see it exists.
+  // We leave this blank so it just loads the live internet version of your app
 });
